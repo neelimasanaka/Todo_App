63c32041207acc397082e74d791f8b199c2e5f8c
Neelima <sanakaneelima15@gmail.com>
2026-05-17 21:34:07 +0530

    Fixed Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Todo_App/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
-})
+}
+)
